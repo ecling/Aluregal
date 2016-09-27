99406076d07b3a42efa15836802d2c739df40313
barrett <735398606@qq.com>
2016-09-27 18:06:14 +0800

    201609271806

--- src/js/function.js
+++ src/js/function.js
@@ -142,15 +142,13 @@
     };
     var whichAnimationEvent = function(){
         var t,
-            el = document.createElement("fakeelement");
-    
+            el = document.createElement("fakeelement");   
         var animations = {
             "animation": "animationend",
             "OAnimation": "oAnimationEnd",
             "MozAnimation": "animationend",
             "WebkitAnimation": "webkitAnimationEnd"
-        }
-    
+        }  
         for (t in animations) {
             if (el.style[t] !== undefined) {
                 return animations[t];
@@ -203,7 +201,6 @@
                 event.stopPropagation();
                 showUp();
             });
-
             close.click(function(event) {
                 event.preventDefault();
                 event.stopPropagation();
